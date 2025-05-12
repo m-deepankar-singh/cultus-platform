15e03bc8a0e511e9f64905af46d86cc158a09f92
m-deepankar-singh <singh.deepankar39@gmail.com>
2025-05-12 17:40:09 +0530

    fixed admin button redirect

--- app/page.tsx
+++ app/page.tsx
@@ -8,7 +8,7 @@ export default function HomePage() {
     <main>
         {/* Container for top-right navigation buttons */}
       <div className="absolute top-4 right-4 z-20 flex gap-4">
-        <Link href="/dashboard" passHref>
+        <Link href="/admin/login" passHref>
           <span className="px-4 py-2 bg-white/20 hover:bg-white/30 backdrop-blur-md rounded-md text-black dark:text-white font-medium transition-all duration-200">
             Admin
           </span>
